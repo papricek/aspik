e76a1c174da930162aad14b236d263d79589008f
Patrik <patrikjira@gmail.com>
2008-12-30 16:32:41 +0100

    Ladeni deploymentu

--- Rakefile
+++ Rakefile
@@ -3,29 +3,4 @@ require 'activerecord'
 require 'rake'
 require 'ftools'
 
-namespace :app do
-  namespace :install do
 
-    require 'config/deploy'
-    
-    task :create_data_directory do
-      puts "Creating data directory in " + data_directory
-      FileUtils.mkdir_p( data_directory )
-    end
-
-    task :create_links do
-      run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml "
-      run "ln -nfs #{deploy_to}/shared/data/database.db #{release_path}/config/database.db "
-    end
-
-
-
-    task :create_database do
-      puts "Creating database in #{data_directory}/database.db"
-      ActiveRecord::Base.establish_connection( :adapter => 'sqlite3',
-        :database => File.join(data_directory, 'database.db')
-      )
-      load( File.join( File.dirname(__FILE__), 'config', 'migrations.rb' ) )
-    end
-  end
-end
--- config/deploy.rb
+++ config/deploy.rb
@@ -45,3 +45,30 @@ after :deploy do
   run "cd #{current_path}; rake app:install:create_links"
 end
 
+
+namespace :app do
+  namespace :install do
+
+    require 'config/deploy'
+
+    task :create_data_directory do
+      puts "Creating data directory in " + data_directory
+      FileUtils.mkdir_p( data_directory )
+    end
+
+    task :create_links do
+      run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml "
+      run "ln -nfs #{deploy_to}/shared/data/database.db #{release_path}/config/database.db "
+    end
+
+
+
+    task :create_database do
+      puts "Creating database in #{data_directory}/database.db"
+      ActiveRecord::Base.establish_connection( :adapter => 'sqlite3',
+        :database => File.join(data_directory, 'database.db')
+      )
+      load( File.join( File.dirname(__FILE__), 'config', 'migrations.rb' ) )
+    end
+  end
+end
