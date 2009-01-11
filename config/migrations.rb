4c30e2bf415251b72062b0e93ef8ca512a2f532b
unknown <fafa@.(none)>
2009-01-11 15:34:47 +0100

    Cachry s cestou k databazi

--- config/migrations.rb
+++ config/migrations.rb
@@ -5,6 +5,9 @@ load(File.join( File.dirname(__FILE__), '..', 'app', 'configuration.rb' ))
 begin
   ActiveRecord::Base.establish_connection(:adapter => "sqlite3", 
     :database => File.join( File.dirname(__FILE__), '..', 'data', 'database.rb' ))
+  puts File.join( File.dirname(__FILE__), '..', 'data', 'database.rb' )
+
+
   ActiveRecord::Schema.define(:version => 1) do
     create_table :articles do |t|
       t.string :title
